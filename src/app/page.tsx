aa65196f6454877b58c7768fc0ecfedfdb8b99e6
Katherine <katerynaunik@gmail.com>
2026-05-28 15:28:16 +0300

    menu html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
+    <div>
+      <div className="flex flex-col">
+        <menu className="flex flex-row">
+          <div>
+            <Link href="/">
+            <Image
+            src="/loader/loader_logo.png"
+            alt="LumiTune"
+            width={150}
+            height={150}
+            />
+            </Link>
+          </div>
+
+          <div className="flex">
+            <ul className="flex flex-row">
+              <li>Підписки</li>
+              <li>Підтримка</li>
+              <li>Завантажити додаток</li>
+            </ul>
+            <button>Реєстрація</button>
+          </div>
+        </menu>
+      </div>
     </div>
   );
 }
