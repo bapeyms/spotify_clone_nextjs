b36ac4e273dece7d9c336b4449941c22c0c6c494
Katherine <katerynaunik@gmail.com>
2026-05-30 01:05:58 +0300

    first page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,42 +4,47 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col bg-[#061420] w-full h-screen">
-        <header className="flex flex-row text-[#A6DAFF] border border-red">
-          <div>
+      <div className="flex flex-col bg-[#061420] w-full h-screen font-sans">
+        <header className="flex flex-row justify-between items-center text-[#A6DAFF] px-3 border-b border-[#93E8FF]/30">
+          <div className=" text-center">
             <Link href="/">
             <Image
             src="/loader/loader_logo.png"
             alt="LumiTune"
             width={150}
             height={150}
+            className="mt-1"
             />
             </Link>
           </div>
 
-          <nav className="flex">
-            <ul className="flex flex-row">
-              <li>Підписки</li>
-              <li>Підтримка</li>
-              <li>Завантажити додаток</li>
+          <nav className="flex gap-7 font-semibold">
+            <ul className="flex flex-row gap-4 text-[#88BCD6]">
+              <li className="hover:text-[#2BD9F2] transition-colors cursor-pointer">Підписки</li>
+              <li className="hover:text-[#2BD9F2] transition-colors cursor-pointer">Підтримка</li>
+              <li className="hover:text-[#2BD9F2] transition-colors cursor-pointer">Завантажити додаток</li>
             </ul>
-            <button>Реєстрація</button>
+            <button className="border border-[#93E8FF] px-3 py-1 rounded hover:bg-[#93E8FF] hover:text-[#061420] transition-all duration-200">Реєстрація</button>
           </nav>
         </header>
 
-        <div className="border border-pink-400">
+        <div className="flex flex-col relative w-full h-screen bg-[#061420] z-0">
           <img src="/loader/background_gradient.svg"
-          className="absolute inset-0 w-full h-full object-cover"/>
+          className="absolute -z-10 inset-0 w-full h-full object-cover pointer-events-none"/>
           <img src="/loader/background_figures.svg"
-          className="absolute inset-0 w-full h-full object-cover"/>
-          <div>
-            <img></img>
-            <h1 className="text-[#AFF0F2]">Закортилося чогось новенького у рутині?</h1>
-            <p className="text-[#9AB5FFB2]">Мерщій приєднуйся до шабашу музик! Тут звучать ритми, історії!</p>
-          </div>
-          <div>
-            <button className="bg-[#02192E] text-[#2BD9F2]">Перейти на сайт</button>
-            <button className="bg-[#2BD9F2] text-[#02192E]">Зареєструватися</button>
+          className="absolute inset-0 w-full h-full object-cover -z-10"/>
+          
+          <div className="relative z-10 flex flex-col w-95 h-auto gap-1 mt-15 lg:ml-35">
+            <img src="/loader/lumitune.png"
+            className="w-96 h-auto"></img>
+            <h1 className="text-[#AFF0F2] text-4xl font-semibold text-center">Закортілося чогось новенького у рутині?</h1>
+            <p className="text-[#9AB5FFB2] text-center">Мерщій приєднуйся до шабашу музик! Тут звучать ритми, історії!</p>
+            
+            <div className="flex flex-row items-center justify-around mt-2">
+              <button className="bg-[#02192E] text-[#2BD9F2] px-3 py-2 rounded-lg font-semibold">Перейти на сайт</button>
+              <button className="bg-[#2BD9F2] text-[#02192E] px-3 py-2 rounded-lg font-semibold">Зареєструватися</button>
+            </div>
+          
           </div>
         </div>
       </div>
