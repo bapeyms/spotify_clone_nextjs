4e4b8aba81c55f1e5ed3395b3a3dad63a0940d1d
Katherine <katerynaunik@gmail.com>
2026-06-01 23:40:37 +0300

    added adaptation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,24 +28,33 @@ export default function Home() {
           </nav>
         </header>
 
-        <div className="flex flex-row relative w-full h-screen bg-[#061420] z-0">
+        <div className="flex flex-col md:flex-row lg:flex-row items-center relative w-full h-screen bg-[#061420] z-0">
           <img src="/loader/bg1.png"
           className="absolute -z-10 inset-0 w-full h-full object-cover"/>
           
-          <div className="relative z-10 flex flex-col w-95 h-auto gap-1 mt-15 lg:ml-35">
-            <img src="/loader/lumitune.png"
-            className="w-96 h-auto"></img>
-            <h1 className="text-[#AFF0F2] text-4xl font-semibold text-center">Закортілося чогось новенького у рутині?</h1>
-            <p className="text-[#9AB5FFB2] text-center">Мерщій приєднуйся до шабашу музик! Тут звучать ритми, історії!</p>
+          <div className="flex flex-col flex-1 border border-red-100 w-full h-full">
             
-            <div className="flex flex-row items-center justify-around mt-2">
-              <button className="bg-[#02192E] text-[#2BD9F2] px-3 py-2 rounded-lg font-semibold">Перейти на сайт</button>
-              <button className="bg-[#2BD9F2] text-[#02192E] px-3 py-2 rounded-lg font-semibold">Зареєструватися</button>
+            <div className="flex flex-col items-center">
+              <div className="border border-red-900 flex flex-col w-3/5 lg:w-3/4 items-center mt-26 lg:mt-8 mb-2">
+                <img src="/loader/lumitune.png"
+                className="w-full h-auto"></img>
+                <h1 className="text-[#AFF0F2] w-full h-auto text-2xl lg:text-3xl font-semibold pl-6 lg:pl-4 mb-2 -mt-10 lg:-mt-12 text-center lg:text-left">Закортілося чогось новенького у рутині?</h1>
+                <p className="text-[#9AB5FFB2] w-full h-auto text-xs lg:text-sm pl-6 lg:pl-4 mb-2 text-center lg:text-left">Мерщій приєднуйся до шабашу музики! Тут звучать ритми, історії!</p>
+              </div>
+              
+              <div className="border border-red-900 flex flex-col lg:flex-row w-3/5 lg:w-3/4 items-center justify-center gap-2 lg:gap-6 mt-2">
+                <button className="bg-[#02192E] text-[#2BD9F2] w-full lg:w-1/2 py-2 rounded-lg font-semibold
+                hover:bg-[#2BD9F2] hover:text-[#02192E] transition-all cursor-pointer">Перейти на сайт</button>
+                <button className="bg-[#2BD9F2] text-[#02192E] w-full lg:w-1/2 py-2 rounded-lg font-semibold
+                hover:bg-[#02192E] hover:text-[#2BD9F2] transition-all cursor-pointer">Зареєструватися</button>
+              </div>
             </div>
+
           </div>
 
-          <div>
-            <img src="/loader/character1.png"></img>
+          <div className="hidden lg:flex items-end justify-center w-full lg:w-1/2 h-full">
+            <img src="/loader/character1.png"
+            className="w-full h-auto object-contain"></img>
           </div>
         </div>
       </div>
