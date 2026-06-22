a29486d5d4977e7c0443f25fa8f4907f07f714ea
Katherine <katerynaunik@gmail.com>
2026-06-22 15:15:44 +0300

    duration added

--- src/data/homepage/your_music_today.ts
+++ src/data/homepage/your_music_today.ts
@@ -2,15 +2,16 @@ export interface YourMusicToday {
   id: string;
   music: string;
   artist: string;
+  duration: string;
   img: string
 }
 
 export const MUSIC_TODAY: YourMusicToday[] = [
-    {id: "1", music: "Die with a smile", artist: "Lady Gaga & Bruno Mars", img: "/dashboard/pagehome/gaga_bruno.png"},
-    {id: "2", music: "Глубоко", artist: "Monatik & Надія Дорофєєва", img: "/dashboard/pagehome/dodo_monatik.png"},
-    {id: "3", music: "Superman", artist: "Eminiem", img: "/dashboard/pagehome/eminem.png"},
-    {id: "4", music: "Sweater Weather", artist: "The Neighberhood", img: "/dashboard/pagehome/neighbourhood.png"},
-    {id: "5", music: "Cry Me A River", artist: "Justin Timberlake", img: "/dashboard/pagehome/timberlake.png"},
-    {id: "6", music: "Do I Wanna Know", artist: "Arctic Monkeys", img: "/dashboard/pagehome/monkeys1.png"},
-    {id: "7", music: "Too Much To Ask", artist: "Arctic Monkeys", img: "/dashboard/pagehome/monkeys2.png"}
+    {id: "1", music: "Die with a smile", artist: "Lady Gaga & Bruno Mars", duration: "4:11", img: "/dashboard/pagehome/gaga_bruno.png"},
+    {id: "2", music: "Глубоко...", artist: "Monatik & Надія Дорофєєва", duration: "4:11", img: "/dashboard/pagehome/dodo_monatik.png"},
+    {id: "3", music: "Superman", artist: "Eminiem", duration: "5:50", img: "/dashboard/pagehome/eminem.png"},
+    {id: "4", music: "Sweater Weather", artist: "The Neighberhood", duration: "3:59", img: "/dashboard/pagehome/neighbourhood.png"},
+    {id: "5", music: "Cry Me A River", artist: "Justin Timberlake", duration: "4:47", img: "/dashboard/pagehome/timberlake.png"},
+    {id: "6", music: "Do I Wanna Know", artist: "Arctic Monkeys", duration: "4:31", img: "/dashboard/pagehome/monkeys1.png"},
+    {id: "7", music: "Too Much To Ask", artist: "Arctic Monkeys", duration: "3:02", img: "/dashboard/pagehome/monkeys2.png"}
 ]
